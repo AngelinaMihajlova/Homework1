082a77922f493c324fc8f676f900cf7a9b670985
AngelinaMihajlova <mihajlovaangelina@yandex.ru>
2022-06-30 14:34:09 +0300

    Добавлены изменения

---
+++ Task001_MaxMin/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1
+int a = 5;
+int b = 7;
+int max = a;
+int min = b;
+if (a > b) 
+{
+    max = a; min = b;
+}
+else
+{
+    max = b; min = a;
+}
+Console.Write ("max =  ");
+Console.WriteLine (max);
+Console.Write ("min =  ");
+Console.WriteLine (min);
