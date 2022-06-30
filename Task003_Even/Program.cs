5cec8562bbc26e0af2ff1d673c265550a45df1fc
AngelinaMihajlova <mihajlovaangelina@yandex.ru>
2022-06-30 16:27:29 +0300

    Задача на определение четного числа

---
+++ Task003_Even/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 3 Проверка числа на четность
+int a = 4;
+int b = -3;
+int c = 7;
+if (a % 2 == 0) 
+{
+    Console.Write ("Четное число = ");
+    Console.WriteLine (a);
+}
+else
+{
+    Console.Write ("Нечетное число = ");
+    Console.WriteLine (a);
+}
+if (b % 2 == 0) 
+{
+    Console.Write ("Четное число = ");
+    Console.WriteLine (b);
+}
+else
+{
+    Console.Write ("Нечетное число = ");
+    Console.WriteLine (b);
+}
+if (c % 2 == 0) 
+{
+    Console.Write ("Четное число = ");
+    Console.WriteLine (c);
+}
+else
+{
+    Console.Write ("Нечетное число = ");
+    Console.WriteLine (c);
+}
